6841e88152b877a4aff8798e7268c5086d5d4960
galmodovar <galmodovar1@gmail.com>
2021-08-05 16:22:27 -0500

    fixed file name in governor module

---
+++ src/scripts/governors.js
@@ -0,0 +1,20 @@
+import { getGovernors } from "./database.js";
+
+export const Governors = () => {
+    const governors = getGovernors();
+    let html = `<select class="governors" id="governors">
+                <option value="">Choose</option>`
+    
+    const govHTML = governors.map(gov => {
+        return `
+        <option value="${gov.id}">${gov.name}</option>
+        `
+                       
+    } )
+
+    html += govHTML.join("")
+    html += "</select>"
+
+    return html
+    
+}
