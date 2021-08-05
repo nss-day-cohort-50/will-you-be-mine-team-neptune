6609448525a55e1bf2ab7b4fc6698d93ade99af4
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-08-05 13:51:25 -0500

    built the mainJS module

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+import { WhistlingWillies } from "./WhistlingWhillies.js"
+
+const mainContainer = document.querySelector("#container")
+
+
+// Below is our event listener for the state change
+document.addEventListener(
+    "stateChanged", 
+    event => {
+        console.log("State of data has changed... regenerating HTML...")
+        renderHTML()
+    }
+)
+
+export const renderHTML = () => {
+    mainContainer.innerHTML = WhistlingWillies()
+}
+
+renderHTML()
+
